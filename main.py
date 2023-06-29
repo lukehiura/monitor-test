c6e66e20348b273c7415eb8a150ed1951365b70f
Luke Hiura <lhiura@sequel.ae>
2023-06-28 19:49:15 -0700

    Reverting back

--- main.py
+++ main.py
@@ -13,20 +13,18 @@ from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 
-# Tracing Configuration
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string="InstrumentationKey=86efe66a-ff4c-40b6-837b-7db9417ca81a;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"
+        connection_string='InstrumentationKey=<your-instrumentation-key>;IngestionEndpoint=<your-ingestion-endpoint>'
     ),
     sampler=ProbabilitySampler(1.0),
 )
 
-# Logging Configuration
-logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string="InstrumentationKey=86efe66a-ff4c-40b6-837b-7db9417ca81a;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"))
-logger.setLevel(logging.INFO)
 
-properties = {'custom_dimensions': {'key_1': 'value_1', 'key_2': 'value_2'}}
+# Logging Configuration
+logger = logging.getLogger()
+logger.addHandler(AzureLogHandler(connection_string="InstrumentationKey=9fecf82f-83e4-4a24-8976-de12ae6f0463;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"))
+logger.setLevel(logging.INFO)
 
 app = FastAPI(title="Exception Catching API", description="API to test and catch exceptions")
 
@@ -48,68 +46,62 @@ async def create_item(item: Item):
     """
     Create an item.
     """
-    with tracer.span(name="create_item"):
-        try:
-            if item.name in items:
-                raise HTTPException(status_code=400, detail="Item already exists")
-            items[item.name] = item
-            logger.info(f"Item created: {item.name}")
-            return item
-        except Exception as e:
-            logger.exception(f"Error creating item: {str(e)}", extra=properties)
-            raise
+    try:
+        if item.name in items:
+            raise HTTPException(status_code=400, detail="Item already exists")
+        items[item.name] = item
+        logger.info(f"Item created: {item.name}")
+        return item
+    except Exception as e:
+        logger.error(f"Error creating item: {str(e)}")
+        raise
 
 @app.get("/items/{item_name}")
 async def read_item(item_name: str):
     """
     Read an item by name.
     """
-    with tracer.span(name="read_item"):
-        try:
-            if item_name not in items:
-                raise HTTPException(status_code=404, detail="Item not found")
-            logger.info(f"Item read: {item_name}")
-            return items[item_name]
-        except Exception as e:
-            logger.error(f"Error reading item: {str(e)}")
-            raise
+    try:
+        if item_name not in items:
+            raise HTTPException(status_code=404, detail="Item not found")
+        logger.info(f"Item read: {item_name}")
+        return items[item_name]
+    except Exception as e:
+        logger.error(f"Error reading item: {str(e)}")
+        raise
 
 @app.get("/divide-by-zero")
 async def divide_by_zero():
     """
     Generate a ZeroDivisionError.
     """
-    with tracer.span(name="divide_by_zero"):
-        properties = {'custom_dimensions': {'key_1': 'value_1', 'key_2': 'value_2'}}
-        try:
-            result = 1 / 0  # Generate a ZeroDivisionError
-        except Exception:
-            logger.exception('Captured an exception.', extra=properties)
-            raise
-
+    properties = {'custom_dimensions': {'key_1': 'value_1', 'key_2': 'value_2'}}
+    try:
+        result = 1 / 0  # Generate a ZeroDivisionError
+    except Exception:
+        logger.exception('Captured an exception.', extra=properties)
+        raise
 
 @app.get("/key-error")
 async def key_error():
     """
     Generate a KeyError.
     """
-    with tracer.span(name="key_error"):
-        properties = {'custom_dimensions': {'key_1': 'value_1', 'key_2': 'value_2'}}
-        try:
-            dictionary = {}  # An empty dictionary
-            value = dictionary["non_existent_key"]  # Generate a KeyError
-        except Exception:
-            logger.exception('Captured an exception.', extra=properties)
-            raise
+    properties = {'custom_dimensions': {'key_1': 'value_1', 'key_2': 'value_2'}}
+    try:
+        my_dict = {'key': 'value'}
+        value = my_dict['non_existent_key']  # Generate a KeyError
+    except Exception:
+        logger.exception('Captured an exception.', extra=properties)
+        raise
 
-
+# Endpoint for the navigation page
 @app.get("/", response_class=HTMLResponse)
 async def navigation_page(request: Request):
     """
     Render the navigation page.
     """
-    with tracer.span(name="navigation_page"):
-        return templates.TemplateResponse("navigation.html", {"request": request})
+    return templates.TemplateResponse("navigation.html", {"request": request})
 
 # Root endpoint to render the HTML template
 @app.get("/home", response_class=HTMLResponse)
@@ -117,37 +109,33 @@ async def home_page(request: Request):
     """
     Render the home page.
     """
-    with tracer.span(name="home_page"):
-        return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 # Generate OpenAPI schema
 def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
-    with tracer.span(name="custom_openapi"):
-        openapi_schema = get_openapi(
-            title=app.title,
-            version=app.version,
-            description=app.description,
-            routes=app.routes,
-        )
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
     app.openapi_schema = openapi_schema
     return app.openapi_schema
 
 # Route for serving OpenAPI JSON schema
 @app.get("/openapi.json")
 def get_openapi_endpoint():
-    with tracer.span(name="get_openapi_endpoint"):
-        return JSONResponse(content=custom_openapi())
+    return JSONResponse(content=custom_openapi())
 
 # Route for serving Swagger UI HTML page
 @app.get("/docs", response_class=HTMLResponse)
 def swagger_ui_html():
-    with tracer.span(name="swagger_ui_html"):
-        return get_swagger_ui_html(
-            openapi_url="/openapi.json",
-            title=app.title,
-        )
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title=app.title,
+    )
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
